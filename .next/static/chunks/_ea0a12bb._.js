3af4b0c28fedf639fb2e019aa6c55a4593352646
vvvemanresuaretne <vvvasilkovskyi@gmail.com>
2025-07-28 18:29:49 +0200

    update

--- .next/static/chunks/_ea0a12bb._.js
+++ .next/static/chunks/_ea0a12bb._.js
@@ -11,7 +11,7 @@ __turbopack_context__.s({
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$build$2f$polyfills$2f$process$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/build/polyfills/process.js [app-client] (ecmascript)");
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$axios$2f$lib$2f$axios$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/axios/lib/axios.js [app-client] (ecmascript)");
 ;
-const baseURL = ("TURBOPACK compile-time value", "https://09-auth-final-hw.vercel.app/") + "/api";
+const baseURL = ("TURBOPACK compile-time value", "https://09-auth-final-hw.vercel.app") + "/api";
 const nextServer = __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$axios$2f$lib$2f$axios$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"].create({
     baseURL,
     withCredentials: true
